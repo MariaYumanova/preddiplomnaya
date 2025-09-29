15b244406cc499b700d0d15ade656a6b4b016024
MariaYumanova <yuuu.mariii@gmail.com>
2025-09-29 19:07:31 +0400

    Update file_manager.php

--- my-account/file_manager.php
+++ my-account/file_manager.php
@@ -280,7 +280,7 @@ function formatSize($bytes) {
     														<h3 class="footer-title">О нас!</h3>
     														<ul class="footer-links">
     																<li><a href="/a5213-contact/">Контакты</a></li>
-    																<li><a href="#">Версия сайта для слабовидящих</a></li>
+    																
     														</ul>
     												</div>
 
@@ -307,3 +307,4 @@ function formatSize($bytes) {
     								</footer>
 </body>
 </html>
+
