709aee66fb25e44d2825eede1f05118ce15652f3
MariaYumanova <yuuu.mariii@gmail.com>
2025-09-29 19:16:25 +0400

    Update index.php

--- опрос-какие-технологии-вам-интересны/index.php
+++ опрос-какие-технологии-вам-интересны/index.php
@@ -152,7 +152,7 @@
                     <h3 class="footer-title">О нас!</h3>
                     <ul class="footer-links">
                         <li><a href="/a5213-contact/">Контакты</a></li>
-                        <li><a href="#">Версия сайта для слабовидящих</a></li>
+                     
                     </ul>
                 </div>
 
@@ -302,3 +302,4 @@
     </script>
 </body>
 </html>
+
